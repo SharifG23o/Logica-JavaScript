f0ace55d40f31d2961e4d4606e75f161e529bf93
SharifG23o <sharifgiraldo23@gmail.com>
2025-01-16 14:48:58 -0500

    App

--- 2034-logica-programacion-1-Aula1/2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/2034-logica-programacion-1-Aula1/app.js
@@ -1,20 +1,51 @@
 // Variables
-let numeroSecreto = 3;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1; 
+//let palabraVeces= "vez";
+let maximosintentos=2;
 
-console.log(numeroUsuario);
 
+while(numeroUsuario!= numeroSecreto){
 
-/**
- * Este código
- * realiza la comparación
- */
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste el numero es: ${numeroUsuario}`);
-}else{
-    //La condición no se cumplió
-    alert("Lo siento, no acertaste el número")
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor: ")
+
+    
+    console.log(numeroUsuario);
+
+    /**
+     * Este código
+     * realiza la comparación
+     * Es un condicional anidado
+     */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos ==1 ? "vez" :"veces"}`);
+    }else{
+        //La condición no se cumplió
+        if(numeroUsuario > numeroSecreto){
+            alert("El número secreto es menor")
+        }else{
+            alert("El número secreto es mayor")
+        }
+
+        //Incrementamos el contador cuando no acierta
+
+        //intentos = intentos +1;
+        //intentos+=1;
+        intentos++;
+        
+
+        //palabraVeces = "veces"
+
+        if (intentos > maximosintentos){
+            alert(`Llegaste al número máximo de ${maximosintentos} intentos`);
+            break;
+            //Rompemos el ciclo
+        }
+      
+        
+    }
+
 }
-
 
