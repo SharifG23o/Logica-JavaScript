8105968b3443ef5de99ac2f2bc44e66f8869c2bc
SharifG23o <sharifgiraldo23@gmail.com>
2025-01-16 14:47:17 -0500

    Condicionales anidados javaScript

---
+++ 2034-logica-programacion-1-Aula1/2034-logica-programacion-1-Aula1/condicionalesAnidados.js
@@ -0,0 +1,25 @@
+// Variables
+let numeroSecreto = 5;
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+
+console.log(numeroUsuario);
+
+
+/**
+ * Este código
+ * realiza la comparación
+ * Es un condicional anidado
+ */
+if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste el numero es: ${numeroUsuario}`);
+}else{
+    //La condición no se cumplió
+    if(numeroUsuario > numeroSecreto){
+        alert("El número secreto es menor")
+    }else{
+        alert("El número secreto es mayor")
+    }
+    
+}
+
